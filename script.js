13fd17c8280775d09c388b0e249b6d7a33ffe633
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-14 00:24:17 -0700

    update_the_js_file

--- script.js
+++ script.js
@@ -4,6 +4,15 @@
 console.log(typeof students);
 
 
+// let students = 
+// {};: This line of code declares a variable named students and initializes it with an empty object ({}).
+//  In JavaScript, objects are a data type that store key-value pairs. In this case, students is being assigned an empty object.
+
+// console.log(typeof students);: 
+// This line uses the console.log() function to print the result of the typeof operator applied to the students variable. The typeof operator returns a string that indicates the data type of the operand.
+
+// When you run this code, you're first declaring the students variable as an empty object. Then, you're printing the result of typeof students, which will give you the string "object". This is because, in JavaScript, objects are considered a type of data structure.
+
 
 
 
